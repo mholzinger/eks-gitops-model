e8fbc11f3a764759883ac83ef87bda45a1d10036
Mike Holzinger <mike.holzinger@gmail.com>
2025-03-03 13:18:11 -0500

    set cluster_endpoint_private_access to true

--- main.tf
+++ main.tf
@@ -9,14 +9,20 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 }
+
 
 module "eks" {
   source            = "terraform-aws-modules/eks/aws"
-  cluster_name      = "my-eks-cluster"
-  cluster_version   = "1.29"  # or 1.30, 1.31, 1.32, etc.
+  version = "~> 20.31"
+  cluster_name      = "eks-gitops-cluster"
+  cluster_version   = "1.30"  # or 1.30, 1.31, 1.32, etc.
   vpc_id            = module.vpc.vpc_id
   subnet_ids        = module.vpc.private_subnets
+  cluster_endpoint_private_access = true
 
   eks_managed_node_groups = {
     default = {
