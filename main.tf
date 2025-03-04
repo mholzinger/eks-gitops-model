480829c76c71d1626fe387fd2824a1e87e164b57
Mike Holzinger <mike.holzinger@gmail.com>
2025-03-04 11:08:57 -0500

    Use upgraded cluster version

--- main.tf
+++ main.tf
@@ -19,10 +19,12 @@ module "eks" {
   source            = "terraform-aws-modules/eks/aws"
   version = "~> 20.31"
   cluster_name      = "eks-gitops-cluster"
-  cluster_version   = "1.30"  # or 1.30, 1.31, 1.32, etc.
+  cluster_version   = "1.31"
   vpc_id            = module.vpc.vpc_id
   subnet_ids        = module.vpc.private_subnets
   cluster_endpoint_private_access = true
+  cluster_endpoint_public_access = true
+
 
   eks_managed_node_groups = {
     default = {
